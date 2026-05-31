9c432f14977164f74c5a2dbc67e38af9190936af
Hector <hulisesbao@outlook.es>
2026-05-31 14:47:16 -0500

    primer commit

---
+++ app/main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="RAG Educational Platform API",
+    description="Backend para la gestión de salas de estudio, documentos y orquestación RAG.",
+    version="1.0.0")
