7ac69b114144e57df20be9bcff951312b862928f
luisangel-98-vazquez <luisangelvazquezc784@gmail.com>
2026-06-23 00:14:46 -0500

    fix archive requirements and test module user with auth

--- alembic/env.py
+++ alembic/env.py
@@ -1,7 +1,10 @@
 import os
+from dotenv import load_dotenv
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
 from alembic import context
+
+load_dotenv() 
 
 # Importa los modelos para que Alembic los detecte
 from app.db.session import Base
@@ -13,6 +16,8 @@ config = context.config
 database_url = os.getenv("DATABASE_URL")
 if database_url:
     config.set_main_option("sqlalchemy.url", database_url)
+else:
+    raise RuntimeError("DATABASE_URL no está definida (revisa tu .env)")
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
